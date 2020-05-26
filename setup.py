59ca88a63ae758d3b5aa91ad3c8d8f4791b39ad0
roibarzur <roi.bar-zur@cs.technion.ac.il>
2020-05-26 19:16:56 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-setup(name="pymdptoolbox",
-      version="4.0-b4-dev",
+setup(name="pymdptoolbox-roi",
+      version="4.1",
       author="Steven A. W. Cordwell",
       author_email="steven.cordwell@uqconnect.edu.au",
       url="https://github.com/sawcordwell/pymdptoolbox",
