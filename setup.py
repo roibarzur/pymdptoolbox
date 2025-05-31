290a19498dbf3dc03ec0ca315473e5502199d080
roibarzur <roi.barzur@gmail.com>
2025-05-31 19:18:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="pymdptoolbox-roi",
-      version="4.1.9",
+      version="4.2.0",
       author="Steven A. W. Cordwell",
       author_email="steven.cordwell@uqconnect.edu.au",
       url="https://github.com/sawcordwell/pymdptoolbox",
